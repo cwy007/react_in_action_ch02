5f04a0c07fe58d7e6f8a85dcbb580ebd02e775ba
chenweiyan <chanweiyan007@gmail.com>
2020-07-12 16:35:42 +0800

    this.props.children, Comment

--- index.js
+++ index.js
@@ -28,7 +28,8 @@ class Post extends React.Component {
           {
             className: 'postBody'
           },
-          this.props.content
+          this.props.content,
+          this.props.children
         )
       )
     );
@@ -41,10 +42,47 @@ Post.propTypes = {
   id: PropTypes.number.isRequired
 };
 
+class Comment extends React.Component {
+  render() {
+    return React.createElement(
+      'div',
+      {
+        className: 'comment'
+      },
+      React.createElement(
+        'h2',
+        {
+          className: 'commentAuthor'
+        },
+        this.props.user,
+        React.createElement(
+          'span',
+          {
+            className: 'commentContent'
+          },
+          this.props.content
+        )
+      )
+    );
+  }
+}
+
+Comment.propTypes = {
+  id: PropTypes.number.isRequired,
+  content: PropTypes.string.isRequired,
+  user: PropTypes.string.isRequired
+};
+
 const App = React.createElement(Post, {
-  id: 1,
-  content: 'said: This is a post!',
-  user: 'mark'
-});
+    id: 1,
+    content: 'said: This is a post!',
+    user: 'mark'
+  },
+  React.createElement(Comment, {
+    id: 2,
+    user: 'bob',
+    content: 'commented: wow! how cool!'
+  })
+);
 
 ReactDOM.render(App, node);
